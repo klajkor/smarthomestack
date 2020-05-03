ef9adeb9de8f4e831ae197cfa5500cd45fa94d97
Robert Klajko <klajkor@gmail.com>
2020-05-03 18:45:23 +0200

    Docker Compose down typo correction

--- 19_docker_compose_init.sh
+++ 19_docker_compose_init.sh
@@ -13,10 +13,13 @@ fi
 docker system prune -f;docker image prune -f;docker volume prune -f
 
 #Initiate downloading of docker images from Docker Hub
+docker-compose -f ~/smarthomestack/docker-compose.yml pull
+
+#First run, errors messages expected due to lack of propoer config files
 docker-compose -f ~/smarthomestack/docker-compose.yml up -d
 
 #Stopping all containers
-docker-compose -f ~/smarthomestack/docker-compose.yml down -d
+docker-compose -f ~/smarthomestack/docker-compose.yml down
 
 #clean-up
 docker system prune -f;docker image prune -f;docker volume prune -f

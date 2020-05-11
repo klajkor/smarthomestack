700b8dfaa0d18cd30d4ea5eedf3fdec1da315b79
Robert Klajko <klajkor@gmail.com>
2020-05-11 07:52:28 +0200

    Telegraf config update

--- 21_telegraf_config.sh
+++ 21_telegraf_config.sh
@@ -9,8 +9,8 @@ set -o allexport
 source influxdb.env
 set +o allexport
 
-docker run --rm telegraf telegraf config > ~/smarthomestack/telegraf/telegraf.conf
+docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d telegraf
+docker run --rm telegraf telegraf config > ~/smarthomestack/telegraf/telegraf.conf
 
 docker-compose -f docker-compose.yml down

8d0793dc1aa103c5721b9baa7a2d183488a3f4a8
Robert Klajko <klajkor@gmail.com>
2020-05-16 18:05:24 +0200

    Mosquitto config script

---
+++ 22_mosquitto_config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Initial config of Mosquitto
+#
+
+STACKDIR=${HOME}/smarthomestack
+
+cd ${STACKDIR}
+
+if [ ! -e mqtt.env ]
+then
+    echo "Please create proper mqtt.env file!"
+    exit 1
+fi
+
+set -o allexport
+source mqtt.env
+set +o allexport
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d influxdb
+
+docker-compose exec mosquitto sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
+docker-compose exec mosquitto sh -c "cat /mosquitto/config/passwd"
+
+docker-compose -f docker-compose.yml down
+
+echo 'mqtt: !include mqtt_config.yaml' >> ${STACKDIR}/homeassistant/configuration.yaml
+echo 'sensor: !include sensor_config.yaml' >> ${STACKDIR}/homeassistant/configuration.yaml
+
+docker-compose -f docker-compose.yml down

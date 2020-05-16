e81b4167fda6c6e33fcffea545987631bd3bf23c
Robert Klajko <klajkor@gmail.com>
2020-05-16 22:11:37 +0200

    compose init update

--- 19_docker_compose_init.sh
+++ 19_docker_compose_init.sh
@@ -3,7 +3,9 @@
 # Initial config of Docker Compose
 #
 
-cd ~/smarthomestack
+STACKDIR=${HOME}/smarthomestack
+
+cd ${STACKDIR}
 if [ ! -e pgsql.env ]
 then
     cp pgsql.env-example pgsql.env
@@ -26,13 +28,13 @@ fi
 docker system prune -f;docker image prune -f;docker volume prune -f
 
 #Initiate downloading of docker images from Docker Hub
-docker-compose -f ~/smarthomestack/docker-compose.yml pull
+docker-compose -f ${STACKDIR}/docker-compose.yml pull
 
 #First run, errors messages expected due to lack of propoer config files
-docker-compose -f ~/smarthomestack/docker-compose.yml up -d
+docker-compose -f ${STACKDIR}/docker-compose.yml up -d
 
 #Stopping all containers
-docker-compose -f ~/smarthomestack/docker-compose.yml down
+docker-compose -f ${STACKDIR}/docker-compose.yml down
 
 #clean-up
 docker system prune -f;docker image prune -f;docker volume prune -f

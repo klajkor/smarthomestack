6a1f0e0abdbdc57bbc6f587702d0a723adde26b2
Robert Klajko <klajkor@gmail.com>
2020-05-16 21:03:11 +0200

    Mosquitto config update

--- 22_mosquitto_config.sh
+++ 22_mosquitto_config.sh
@@ -19,13 +19,13 @@ set +o allexport
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d mosquitto
+#docker-compose -f docker-compose.yml up -d mosquitto
 
-#docker run --rm eclipse-mosquitto sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
-#docker run --rm eclipse-mosquitto sh -c "cat /mosquitto/config/passwd"
+docker run --rm -v ${STACKDIR}/mosquitto/config:/mosquitto/config -v ${STACKDIR}/mosquitto/log:/mosquitto/log eclipse-mosquitto sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
+docker run --rm -v ${STACKDIR}/mosquitto/config:/mosquitto/config -v ${STACKDIR}/mosquitto/log:/mosquitto/log eclipse-mosquitto sh -c "cat /mosquitto/config/passwd"
 
-docker-compose exec mosquitto sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
-docker-compose exec mosquitto sh -c "cat /mosquitto/config/passwd"
+#docker-compose exec mosquitto sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
+#docker-compose exec mosquitto sh -c "cat /mosquitto/config/passwd"
 
 docker-compose -f docker-compose.yml down
 

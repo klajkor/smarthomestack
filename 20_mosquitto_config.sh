fb64e81f1da8582487f8ef12c902545cb6ff965a
Robert Klajko <klajkor@gmail.com>
2020-05-17 00:47:02 +0200

    Mosquitto config script rename and update

---
+++ 20_mosquitto_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Initial config of Mosquitto
+#
+
+STACKDIR=${HOME}/smarthomestack
+
+cd ${STACKDIR}
+
+if [ ! -e mqtt.env ]
+then
+    echo "Please create proper mqtt.env file!"
+    exit 1
+fi
+
+set -o allexport
+source mqtt.env
+set +o allexport
+
+echo "Shutting down container(s)"
+docker-compose -f docker-compose.yml down
+
+mosquitto_image=`grep "image: eclipse-mosquitto" docker-compose.yml | awk -F":" '{print $2":"$3}'`
+
+echo "Making sure that there is a passwd and log file"
+touch ${STACKDIR}/mosquitto/config/passwd
+touch ${STACKDIR}/mosquitto/log/mosquitto.log
+
+echo "Setting up password for image ${mosquitto_image}"
+docker run --rm -v ${STACKDIR}/mosquitto/config:/mosquitto/config -v ${STACKDIR}/mosquitto/log:/mosquitto/log ${mosquitto_image} sh -c "mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER} ${MQTT_PASSWORD}"
+
+echo "/mosquitto/config/passwd:"
+docker run --rm -v ${STACKDIR}/mosquitto/config:/mosquitto/config -v ${STACKDIR}/mosquitto/log:/mosquitto/log ${mosquitto_image} sh -c "cat /mosquitto/config/passwd"
+
+echo "Starting up image ${mosquitto_image}"
+docker-compose -f docker-compose.yml up -d mosquitto
+
+echo "Show some logs"
+docker-compose -f docker-compose.yml logs --tail=50
+
+echo "Shutting down container(s)"
+docker-compose -f docker-compose.yml down
+
+echo 'mqtt: !include mqtt_config.yaml' >> ${STACKDIR}/homeassistant/configuration.yaml
+echo 'sensor: !include sensor_config.yaml' >> ${STACKDIR}/homeassistant/configuration.yaml
+

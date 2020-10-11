ea55d1852ddf6029e99a9b758bb933f2c9f13feb
Robert Klajko <klajkor@gmail.com>
2020-10-11 22:47:04 +0200

    MQTT service test update

--- mqtt_service_test.sh
+++ mqtt_service_test.sh
@@ -7,7 +7,11 @@ echo "### Testing MQTT Service"
 NETSTAT_MQTT=`netstat -an --tcp|grep -v '1883[0123456789]'|grep 1883|wc -l`
 if [ $NETSTAT_MQTT -gt 0 ] ; then
   echo "MQTT service is running:"
+  echo " "
+  echo "### netstat:"
   netstat -an --tcp|grep -v '1883[0123456789]'|grep 1883
+  echo " "
+  echo "### process list:"
   ps -fl -C mosquitto
 else
   echo "WARNING: MQTT service is NOT running!"

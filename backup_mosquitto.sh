6a5b9bf15d509170680076db527ac66a1575ad41
Robert Klajko <klajkor@gmail.com>
2021-01-02 21:10:11 +0100

    backup script variable nem correction

--- backup_mosquitto.sh
+++ backup_mosquitto.sh
@@ -2,10 +2,10 @@
 #
 # Mosquitto backup
 #
-SYSTEM_NAME = "mosquitto"
-SOURCE_DIR ="${SYSTEM_NAME}/"
-BACKUP_DIR = "./backup"
-TIME_STAMP = `date "+%Y%m%d-%H%M%S"`
-BACKUP_FILE = "${BACKUP_DIR}/${SYSTEM_NAME}_${TIME_STAMP}"
-mdir -p $BACKUP_DIR
-tar cvfz $BACKUP_FILE $SOURCE_DIR
+SYSTEMNAME = "mosquitto"
+SOURCEDIR ="${SYSTEMNAME}/"
+BACKUPDIR = "./backup"
+TIMESTAMP = `date "+%Y%m%d-%H%M%S"`
+BACKUPFILE = "${BACKUPDIR}/${SYSTEMNAME}_${TIMESTAMP}"
+mdir -p $BACKUPDIR
+tar cvfz $BACKUPFILE $SOURCEDIR

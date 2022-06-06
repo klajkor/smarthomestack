c43fd12cbc1bb2633932d6faea3fe16d743d5834
Robert Klajko <klajkor@gmail.com>
2022-06-06 15:52:44 +0200

    Admin scripts updated to use env file variables

--- adm_influxdb_create_retention.sh
+++ adm_influxdb_create_retention.sh
@@ -3,32 +3,13 @@
 # Testing Influxdb - container must be in running state
 #
 
-echo "=> Testing Influxdb - container must be in running state"
-
-if [ ! -e .env ]
-then
-    echo "<!> Please create proper .env file! Exiting."
-    exit 1
-fi
-
-if [ ! -e influxdb.env ]
-then
-    echo "<!> Please create proper influxdb.env file! Exiting."
-    exit 1
-fi
+cd ~/smarthomestack
 
 set -o allexport
-source .env
 source influxdb.env
 set +o allexport
-
-cd ${STACKDIR}
-
-echo `pwd`
 echo "Compose up influxdb"
-${COMPOSECOMMAND} -f docker-compose.yml up -d influxdb
-echo "Wait for a sec, at least"
-sleep 2
+docker-compose -f docker-compose.yml up -d influxdb
 
 curl -G http://localhost:8086/query  --data-urlencode "u=${INFLUXDB_ADMIN_USER}" --data-urlencode "p=${INFLUXDB_ADMIN_PASSWORD}" --data-urlencode "q=SHOW DATABASES"
 
@@ -37,14 +18,12 @@ echo "--------------"
 docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
 SHOW DATABASES"
 
-echo "SHOW SERIES"
+echo "CREATE 2 year retention policy"
+echo "--------------"
+docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
+CREATE RETENTION POLICY TwoYearHistorical ON sensors DURATION 104w REPLICATION 1"
+
+echo "SHOW Retention Policy"
 echo "-----------"
 docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
-SHOW SERIES"
-
-echo "Show last 20 values"
-echo "-------------------"
-docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
-precision rfc3339
-SELECT * FROM mqtt_consumer ORDER BY time DESC LIMIT 20"
-echo "-------------------"
+SHOW RETENTION POLICIES"
--- adm_influxdb_show_retention.sh
+++ adm_influxdb_show_retention.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Testing Influxdb - container must be in running state
+# Show retention policies of Influxdb - container must be in running state
 #
 
-echo "=> Testing Influxdb - container must be in running state"
+echo "=> Show retention policies of Influxdb - container must be in running state"
 
 if [ ! -e .env ]
 then
@@ -24,11 +24,8 @@ set +o allexport
 
 cd ${STACKDIR}
 
-echo `pwd`
 echo "Compose up influxdb"
 ${COMPOSECOMMAND} -f docker-compose.yml up -d influxdb
-echo "Wait for a sec, at least"
-sleep 2
 
 curl -G http://localhost:8086/query  --data-urlencode "u=${INFLUXDB_ADMIN_USER}" --data-urlencode "p=${INFLUXDB_ADMIN_PASSWORD}" --data-urlencode "q=SHOW DATABASES"
 
@@ -37,14 +34,7 @@ echo "--------------"
 docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
 SHOW DATABASES"
 
-echo "SHOW SERIES"
+echo "SHOW Retention Policy"
 echo "-----------"
 docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
-SHOW SERIES"
-
-echo "Show last 20 values"
-echo "-------------------"
-docker exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors
-precision rfc3339
-SELECT * FROM mqtt_consumer ORDER BY time DESC LIMIT 20"
-echo "-------------------"
+SHOW RETENTION POLICIES"

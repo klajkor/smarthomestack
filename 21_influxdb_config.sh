9c66a7705f63a00e7ec005bf98d25df8d2cf17f1
Robert Klajko <klajkor@gmail.com>
2022-06-07 23:02:01 +0200

    Refactor of telegraf logging

--- 21_influxdb_config.sh
+++ 21_influxdb_config.sh
@@ -29,26 +29,25 @@ influxdb_image=`grep "image: influxdb" docker-compose.yml | awk -F":" '{print $2
 
 ${COMPOSECOMMAND} -f docker-compose.yml down
 
-echo "Creating influxdb.conf for image ${influxdb_image}"
+echo "=> Creating influxdb.conf for image ${influxdb_image}"
 docker run --rm -p 8086:8086 ${influxdb_image} influxd config > ${STACKDIR}/influxdb/influxdb.conf
 read -p "=> Press any key to continue"
 
-echo "Changing influxdb.conf file"
+echo "=> Changing influxdb.conf file"
 sed -i 's/^\(  auth-enabled\s*=\s*\).*$/\1true/' ${STACKDIR}/influxdb/influxdb.conf
 sudo chown -R ${USER}:docker ${STACKDIR}/influxdb/influxdb.conf
 read -p "=> Press any key to continue"
 
-echo "Init influxdb"
+echo "=> Init influxdb"
 docker run --rm -e INFLUXDB_ADMIN_USER=${INFLUXDB_ADMIN_USER} -e INFLUXDB_ADMIN_PASSWORD=${INFLUXDB_ADMIN_PASSWORD} -v ${STACKDIR}/influxdb/db:/var/lib/influxdb ${influxdb_image} /init-influxdb.sh
 read -p "=> Press any key to continue"
 
-echo "Compose up influxdb"
+echo "=> Compose up influxdb"
 ${COMPOSECOMMAND} -f docker-compose.yml up -d influxdb
-read -p "=> Press any key to continue"
-echo "Waiting for 2 sec"
+echo "=> Waiting for 2 sec"
 sleep 2
 
-echo "Show users"
+echo "=> Show users"
 ${COMPOSECOMMAND} exec -it influxdb influx  -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}" -execute 'SHOW USERS'
 read -p "=> Press any key to continue"
 
@@ -57,25 +56,25 @@ read -p "=> Press any key to continue"
 #${COMPOSECOMMAND} exec -it influxdb influx -execute "CREATE USER ${INFLUXDB_ADMIN_USER} WITH PASSWORD '${INFLUXDB_ADMIN_PASSWORD}' WITH ALL PRIVILEGES"
 #read -p "=> Press any key to continue"
 
-echo "Creating sensors DB"
+echo "=> Creating sensors DB"
 ${COMPOSECOMMAND} exec -it influxdb influx  -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}" -execute 'CREATE DATABASE sensors'
 read -p "=> Press any key to continue"
 
-echo "Creating telegraf user"
+echo "=> Creating telegraf user"
 ${COMPOSECOMMAND} exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors  
 CREATE USER ${INFLUXDB_TELEGRAF_USER} WITH PASSWORD '${INFLUXDB_TELEGRAF_PASSWORD}'"
 read -p "=> Press any key to continue"
 
-echo "Granting access to telegraf user"
+echo "=> Granting access to telegraf user"
 ${COMPOSECOMMAND} exec -it influxdb influx -username ${INFLUXDB_ADMIN_USER} -password "${INFLUXDB_ADMIN_PASSWORD}"  -execute "USE sensors  
 GRANT ALL on sensors TO telegraf"
 read -p "=> Press any key to continue"
 
-echo "Show created databases"
+echo "=> Show created databases"
 curl -G http://localhost:8086/query  --data-urlencode "u=${INFLUXDB_ADMIN_USER}" --data-urlencode "p=${INFLUXDB_ADMIN_PASSWORD}" --data-urlencode "q=SHOW DATABASES"
 read -p "=> Press any key to continue"
 
-echo "Compose down"
+echo "=> Compose down"
 ${COMPOSECOMMAND} -f docker-compose.yml down
 
 echo "=> Initial config of Influxdb completed"
--- 22_telegraf_config.sh
+++ 22_telegraf_config.sh
@@ -52,7 +52,7 @@ echo -e '
   debug = true
   quiet = false
   logtarget = "file"
-  logfile = "/telegraf.log"
+  logfile = "/var/log/telegraf/telegraf.log"
   round_interval = true
   metric_batch_size = 1000
   metric_buffer_limit = 10000
